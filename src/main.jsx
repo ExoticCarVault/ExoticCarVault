40686f27ff3114e9bfd271c41365d7b6c1d88672
Juan Anjos <dev-anjos@outlook.com>
2024-08-05 11:00:23 -0300

    Atualizando minha branch de trabalho

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.jsx'
 import './style/index.css'
+import {register} from 'swiper/element/bundle'
+
+register();
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
